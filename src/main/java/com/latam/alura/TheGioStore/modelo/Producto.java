05a0e7dc25e5a27d42d22cb850a13284d1d86602
AndrexMed <giovanniandrex@gmail.com>
2023-06-01 14:05:13 -0500

    Fin mapeo entidad

--- src/main/java/com/latam/alura/TheGioStore/modelo/Producto.java
+++ src/main/java/com/latam/alura/TheGioStore/modelo/Producto.java
@@ -1,7 +1,11 @@
 package com.latam.alura.TheGioStore.modelo;
 
 import java.math.BigDecimal;
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import javax.persistence.Table;
 
 
@@ -11,15 +15,57 @@ import javax.persistence.Table;
  * @author giova
  */
 @Entity
-@Table(name="Productos") //Esto se hace ya que la clase no tiene el mismo nombre de la tabla.
+@Table(name="Productos") //Esto se hace cuando la clase no tiene el mismo nombre de la tabla.
 public class Producto {
 
+    @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY) //Configurando ID para representar llave primaria de la tabla
     private Long idProducto;
+    //@Column(name="nombreProductos") // Esto se hace cuando 
     private String nombrePro;
     private String descripcionPro;
     private int cantidadPro;
     private BigDecimal precioPro;
 
-    
+    public Long getIdProducto() {
+        return idProducto;
+    }
+
+    public void setIdProducto(Long idProducto) {
+        this.idProducto = idProducto;
+    }
+
+    public String getNombrePro() {
+        return nombrePro;
+    }
+
+    public void setNombrePro(String nombrePro) {
+        this.nombrePro = nombrePro;
+    }
+
+    public String getDescripcionPro() {
+        return descripcionPro;
+    }
+
+    public void setDescripcionPro(String descripcionPro) {
+        this.descripcionPro = descripcionPro;
+    }
+
+    public int getCantidadPro() {
+        return cantidadPro;
+    }
+
+    public void setCantidadPro(int cantidadPro) {
+        this.cantidadPro = cantidadPro;
+    }
+
+    public BigDecimal getPrecioPro() {
+        return precioPro;
+    }
+
+    public void setPrecioPro(BigDecimal precioPro) {
+        this.precioPro = precioPro;
+    }
+
     
 }

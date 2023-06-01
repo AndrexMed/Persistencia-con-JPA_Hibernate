3a69f2598befe9203e1b5c27251a6a2681acf60d
AndrexMed <giovanniandrex@gmail.com>
2023-06-01 09:24:47 -0500

    Persistiendo una entidad(tabla) p1

--- src/main/java/com/latam/alura/TheGioStore/modelo/Producto.java
+++ src/main/java/com/latam/alura/TheGioStore/modelo/Producto.java
@@ -1,20 +1,24 @@
 package com.latam.alura.TheGioStore.modelo;
 
+import java.math.BigDecimal;
 import javax.persistence.Entity;
 import javax.persistence.Table;
+
+
 
 /**
  *
  * @author giova
  */
 @Entity
-@Table(name="Productos")
+@Table(name="Productos") //Esto se hace ya que la clase no tiene el mismo nombre de la tabla.
 public class Producto {
 
-    private Long id;
-    private String nombre;
-    private String descripcion;
-    private int cantidad;
+    private Long idProducto;
+    private String nombrePro;
+    private String descripcionPro;
+    private int cantidadPro;
+    private BigDecimal precioPro;
 
     
     

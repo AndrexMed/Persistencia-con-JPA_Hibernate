debb359240a47bf6efee97cb117fc9594bda3666
AndrexMed <giovanniandrex@gmail.com>
2023-06-05 09:36:43 -0500

    Relacionamientos Muchos a Muchos P1

--- src/main/java/com/latam/alura/TheGioStore/modelo/Pedido.java
+++ src/main/java/com/latam/alura/TheGioStore/modelo/Pedido.java
@@ -2,10 +2,13 @@ package com.latam.alura.TheGioStore.modelo;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
+import java.util.List;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
@@ -26,6 +29,10 @@ public class Pedido {
     
     @ManyToOne // Un cliente tiene muchos pedidos... Aqui relacionamos Pedido con Cliente
     private Cliente cliente;
+    
+    @ManyToMany //Muchos Pedidos - Muchos Productos
+    @JoinTable(name="items_pedidox") // Se crea tabla intermedia
+    private List<Producto> productos;
     
     //Constructores
     public Pedido() {
@@ -68,9 +75,5 @@ public class Pedido {
     public void setCliente(Cliente cliente) {
         this.cliente = cliente;
     }
-    
-    
-    
-    
     
 }

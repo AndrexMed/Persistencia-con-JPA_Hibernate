b202c7bc413a1d2d57fb7bc33474f16f0f6f8fb5
AndrexMed <giovanniandrex@gmail.com>
2023-06-05 08:15:22 -0500

    Mapeando Entidades, Relacionandolas

---
+++ src/main/java/com/latam/alura/TheGioStore/modelo/Cliente.java
@@ -0,0 +1,65 @@
+package com.latam.alura.TheGioStore.modelo;
+
+import javax.annotation.processing.Generated;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+/**
+ *
+ * @author giova
+ */
+@Table(name = "Clientes")
+@Entity
+public class Cliente {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long idCliente;
+    private String nombreCliente;
+    private String dni;
+    
+    //Constructores   
+    public Cliente(){
+        
+    }
+
+    public Cliente(Long idCliente, String nombreCliente, String dni) {
+        this.idCliente = idCliente;
+        this.nombreCliente = nombreCliente;
+        this.dni = dni;
+    }
+    
+    //Get&Seters
+
+    public Long getIdCliente() {
+        return idCliente;
+    }
+
+    //public void setIdCliente(Long idCliente) {
+    //    this.idCliente = idCliente;
+    //}
+
+    public String getNombreCliente() {
+        return nombreCliente;
+    }
+
+    public void setNombreCliente(String nombreCliente) {
+        this.nombreCliente = nombreCliente;
+    }
+
+    public String getDni() {
+        return dni;
+    }
+
+    public void setDni(String dni) {
+        this.dni = dni;
+    }
+    
+    
+    
+    
+    
+}
---
+++ src/main/java/com/latam/alura/TheGioStore/modelo/Pedido.java
@@ -0,0 +1,76 @@
+package com.latam.alura.TheGioStore.modelo;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+/**
+ *
+ * @author giova
+ */
+@Table(name="Pedidos")
+@Entity
+public class Pedido {
+    
+    
+    @Id
+    @GeneratedValue( strategy = GenerationType.IDENTITY)
+    private Long idPedido;
+    private LocalDate fechaRegistro = LocalDate.now();
+    private BigDecimal total;
+    
+    @ManyToOne // Un cliente tiene muchos pedidos... Aqui relacionamos Pedido con Cliente
+    private Cliente cliente;
+    
+    //Constructores
+    public Pedido() {
+    }
+
+    public Pedido(Cliente cliente) {
+        this.cliente = cliente;
+    }
+    
+    //Getters y Setters
+
+    public Long getIdPedido() {
+        return idPedido;
+    }
+
+    public void setIdPedido(Long idPedido) {
+        this.idPedido = idPedido;
+    }
+
+    public LocalDate getFechaRegistro() {
+        return fechaRegistro;
+    }
+
+    public void setFechaRegistro(LocalDate fechaRegistro) {
+        this.fechaRegistro = fechaRegistro;
+    }
+
+    public BigDecimal getTotal() {
+        return total;
+    }
+
+    public void setTotal(BigDecimal total) {
+        this.total = total;
+    }
+
+    public Cliente getCliente() {
+        return cliente;
+    }
+
+    public void setCliente(Cliente cliente) {
+        this.cliente = cliente;
+    }
+    
+    
+    
+    
+    
+}
